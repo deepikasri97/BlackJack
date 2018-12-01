9bb083b2dedbd293c1fd16414ef24380acf6e1cc
Cedric <Cedri@LAPTOP-AMM30RG3>
2018-12-01 13:36:10 +0100

    Further finetuning of the class 'Game'. This version buids aswell

--- BlackJack/BlackJack/include/Game.h
+++ BlackJack/BlackJack/include/Game.h
@@ -11,22 +11,21 @@ public:
 	Game();
 	
 	// when game starts, the player should indicate how many hand they want to play
+	// Ask itirate through all players and ask how many hands they watn to play
+	void setTableslots();
 
-	// Subsequently, the game deals two cards to each hand and stores it in private vector 'Hands'
+	// Subsequently, the game deals two cards to each hand and stores it in private vector 'hands'
 	void dealHands();
-
-	// The game then itirates through hands (dealer last)
-	void iterateThroughHands();
 	
-	// In this itiration, the function playHand is called which asks player what they want to do and deals ectra cards 
-	void playHand();
+	// In this itiration, the function playHands is called which asks player what they want to do and deals ectra cards 
+	void playHands();
 
 	~Game();
 private:
 
 	// This is a vector containing all hands (each hand containing two cards and a reference to player)
 	std::vector<Hand> hands;
-	// This vector has a fixed size and in this vecotr we could indicate where slots of the table, player has betted on
+	// This vector has a fixed size and in this vector player could indicate which slots he wants to bet
 	std::vector<Player> tableSlots;
 };
 
--- BlackJack/BlackJack/src/Game.cpp
+++ BlackJack/BlackJack/src/Game.cpp
@@ -5,7 +5,7 @@
 
 Game::Game()
 {
-	// Add hand of dealer in vector 'Hands' when constructer is called
+	// Add hand of dealer in vector 'Hands' when constructer is called?
 }
 
 
@@ -14,16 +14,28 @@ Game::~Game()
 	
 }
 
-void Game::playHand()
+// when game starts, the player should indicate how many hands they want to play
+// Itirate through all players and ask how many hands they want to play
+void Game::setTableslots() 
 {
-	// Ask players questions
-	// Take card out of deck and add cards to hand
-	// Terminate hand
+	// Fill tableSlots vector with players example { Dealer, Player 1, Player 2, Player 2, Player 1 etc }
 }
 
-void Game::iterateThroughHands()
+void Game::dealHands()
 {
-	for (Hand data : hands)
-		std::cout << "Iterating through hand" << std::endl;
+	// itirate though tableSlots backwards
+	//		use getTopTwoCards() in class 'Deck'
+	//		construct object 'hand' by passing trough player and cards
+	//		Push_back hand into std::vector 'Hands'
 }
+
+void Game::playHands()
+{
+	// itirate backwards through std::vector 'hands'
+	//		Given player options: new card and stop
+	//		If new card: getTopCard() in class 'Deck'
+	//		If score exceeds 21: terminate
+	//		Terminate hand
+}
+
 
