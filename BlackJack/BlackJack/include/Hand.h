850bd07a7a41213d0cacb6cd540ca70a4bc9910c
DEEPIKASRI NANDAKUMAR <43956796+deepikasri97@users.noreply.github.com>
2018-12-03 19:39:25 +0100

    Update Hand.h

--- BlackJack/BlackJack/include/Hand.h
+++ BlackJack/BlackJack/include/Hand.h
@@ -3,6 +3,19 @@ class Hand
 {
 public:
 	Hand();
-	~Hand();
+	virtual ~Hand();
+	
+	//adds a card to the hand
+	void Add(Card* pCard);
+	
+	//clears hand of all cards
+	void Clear();
+	
+	//gets hand total value
+	int GetTotal() const;
+	
+private:
+	vector<Card*> m_Cards;
+	
 };
 
