8f96c6faf2d16a67bc8bc422641e3f1182bac63d
DEEPIKASRI NANDAKUMAR <43956796+deepikasri97@users.noreply.github.com>
2018-12-03 19:30:05 +0100

    Update Player.cpp

--- BlackJack/BlackJack/src/Player.cpp
+++ BlackJack/BlackJack/src/Player.cpp
@@ -2,7 +2,9 @@
 
 
 
-Player::Player()
+Player::Player(const string& name):
+Player(name)
+  
 {
 }
 
@@ -10,3 +12,30 @@ Player::Player()
 Player::~Player()
 {
 }
+
+bool Player:: IsHitting() const
+{
+  cout << Name << "Do you Want a hit ? (Y/N): ";
+  char response;
+  cin >> response;
+  return (response == "y' || response == 'Y');
+ }
+ 
+ void Player::Lose() const
+ {
+   cout << Name << "loses \n";
+ }
+       
+ void Player::push() const
+ {
+   cout << Name << "pushes\n";
+ }
+          
+ void Player::IsBusted()const
+ {
+   return (GetTotal()>21);
+ }
+            
+            
+            
+          
