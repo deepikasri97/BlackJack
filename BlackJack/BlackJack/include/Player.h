1862d1b9a34e36c7baaf66a18c1d4e43802b3f79
DEEPIKASRI NANDAKUMAR <43956796+deepikasri97@users.noreply.github.com>
2018-12-03 19:22:52 +0100

    Update Player.h

--- BlackJack/BlackJack/include/Player.h
+++ BlackJack/BlackJack/include/Player.h
@@ -2,7 +2,27 @@
 class Player
 {
 public:
-	Player();
-	~Player();
+	Player(const string& name= " ");
+	virtual ~Player();
+	
+	//returns whether the player wants to hit with another card
+	virtual bool IsHitting() const = 0;
+	
+	//returns whether player has busted - has total greater than 21
+	bool IsBusted() const;
+	 
+	//announces if the player wins
+	void Win() const;
+	
+	//announces if the player loses
+	void Lose() const;
+	
+	//announces if the player pushes
+	void push() const;
+	
+	
+	
+private:
+	string Name;
 };
 
