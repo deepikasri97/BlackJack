2a498c19972b4be3d5a465936b33b7ab9b88b750
DEEPIKASRI NANDAKUMAR <43956796+deepikasri97@users.noreply.github.com>
2018-12-03 20:56:24 +0100

    updating right comments

--- BlackJack/BlackJack/src/Player.cpp
+++ BlackJack/BlackJack/src/Player.cpp
@@ -1,18 +1,23 @@
 #include "Player.h"
 
 
-
+//constructor 
 Player::Player(const string& name):
-Player(name)
+      Player(name)
   
 {
 }
 
-
+//destructor
 Player::~Player()
 {
 }
 
+/*
+* public functions
+*/
+
+//this function checks whether the player hits for another card
 bool Player:: IsHitting() const
 {
   cout << Name << "Do you Want a hit ? (Y/N): ";
@@ -21,21 +26,34 @@ bool Player:: IsHitting() const
   return (response == "y' || response == 'Y');
  }
  
+ //this function calls when the player wins the game
+ void Player::Win() const
+  {
+    cout << Name << "Wins \n";
+  }
+            
+ //this functions calls when the player loses the game         
  void Player::Lose() const
  {
    cout << Name << "loses \n";
  }
-       
+ 
+ //this function calls when the player pushes
  void Player::push() const
  {
    cout << Name << "pushes\n";
  }
-          
- void Player::IsBusted()const
+ 
+ //this function checks whether the player is busted or not
+ void Player::IsBusted() const
  {
    return (GetTotal()>21);
  }
             
-            
+ //this function calls when players busts
+ void Player::Bust() const
+  {
+    cout << Name << "Busts\n";
+  }
             
           
